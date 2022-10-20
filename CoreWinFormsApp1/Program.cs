d3f1a10cfdd0182837d5bcd0870723737ce7861f
clasie <claude_siefers@hotmail.com>
2022-10-20 15:56:19 +0200

    a
    aa

--- CoreWinFormsApp1/Program.cs
+++ CoreWinFormsApp1/Program.cs
@@ -17,7 +17,7 @@ namespace CoreWinFormsApp1
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());//test git csi
+            Application.Run(new Form1());//test git csi 2
         }
     }
 }
